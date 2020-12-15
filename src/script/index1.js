933e59af7735fd3917e35253b2e1c953784bd730
helloleo1123 <2106420709@qq.com>
2020-12-15 13:29:03 +0800

    周二上午

--- src/script/index1.js
+++ src/script/index1.js
@@ -1,26 +1,36 @@
-! function($) {
-    const $index1 = $('.index1 .normal');
-
-    $.ajax({
-        url: 'http://localhost/secondstageproject/php/index1.php',
-        dataType: 'json'
-    }).done(function(data) {
-        let $strhtml = '';
-        $.each(data, function(index, value) {
-            $strhtml += `
-            <div>
-                <a href="detail.html?sid=${value.sid}">
-                    <img src="${value.url}" alt="">
-                </a>
-                <p>
-                    ${value.title}
-                </p>
-                <p>
-                    ￥${value.price}
-                </p>
-            </div>
-            `;
-        });
-        $index1.html($strhtml);
-    });
-}(jQuery);
+define(['jlazyload'], () => {
+    return {
+        init: function() {
+            //渲染+懒加载
+            const $list = $('.dotaarea .goods .right  .normal ');
+            $.ajax({
+                url: 'http://localhost/secondstageproject/php/index1.php',
+                dataType: 'json'
+            }).done(function(data) {
+                let $strhtml = '';
+                $.each(data, function(index, value) {
+                    $strhtml += `
+                    <li>
+                        <a href="detail.html?sid=${value.sid}">
+                            <img src="${value.url}" style="width:160px;height:160px"alt="">
+                        </a>
+                        <p class="goodsname">
+                            ${value.title}
+                        </p>
+                        <p class="goodsprice">
+                            ￥${value.price}
+                        </p>
+                    </li>    
+                    `;
+                });
+                $list.html($strhtml);
+                // 渲染的下面进行懒加载操作
+                $(function() { //页面加载完成
+                    $("img.lazy").lazyload({
+                        effect: "fadeIn" //显示方法：谈入
+                    });
+                });
+            });
+        }
+    }
+});
---
+++ src/script/list.js
@@ -0,0 +1,148 @@
+define(['pagination', 'jlazyload'], function() {
+    return {
+        init: function() {
+            const $list = $('.main .goodslist .wrapper .goodsdetail');
+            let $array_default = []; //排序前的li放入此数组。
+            let $array = []; //排序后的数组
+            let $prev = []; //li里面的商品的前一个价格
+            let $next = []; //li里面的商品的后一个价格
+            //1.渲染列表页面
+            $.ajax({
+                url: 'http://localhost/secondstageproject/php/list.php',
+                dataType: 'json'
+            }).done(function(datalist) {
+                console.log(datalist);
+                data = datalist.pagedata; //获取接口里面数据
+                let $strhtml = '';
+                $.each(data, function(index, value) {
+                    $strhtml += `
+                        <li>
+                            <a href="detail.html?sid=${value.sid}" class="like">
+                                <span></span>
+                                <font></font>
+                            </a>
+                            <a href="">
+                                <img class="lazy" data-original="${value.url}">
+                            </a>
+                            <p class="goodsname">
+                            ${value.title}
+                            </p>
+                            <p class="goodsprice">
+                            ￥${value.price}
+                            </p>
+                        </li>
+                    `;
+                });
+                $list.html($strhtml);
+                //懒加载
+                $("img.lazy").lazyload({ effect: "fadeIn" });
+
+                //将li元素添加到排序前的数组中。
+                $('.list li').each(function(index, element) { //element:原生的元素对象
+                    $array_default[index] = $(this); //排序前
+                    $array[index] = $(this); //排序后
+                });
+                console.log($array_default);
+
+
+                //2.进行分页设置(html页面载入分页的结构)
+                $('.page').pagination({
+                    pageCount: datalist.pageno, //总的页数
+                    jump: true, //是否开启跳转到指定的页数，布尔值。
+                    prevContent: '上一页', //将图标改成上一页下一页。
+                    nextContent: '下一页',
+                    callback: function(api) {
+                        console.log(api.getCurrent()); //获取当前的点击的页码。
+                        $.ajax({
+                            url: 'http://10.31.161.33/secondstageproject/php/list.php',
+                            data: {
+                                page: api.getCurrent()
+                            },
+                            dataType: 'json'
+                        }).done(function(datalist) {
+                            data = datalist.pagedata; //获取接口里面数据
+                            let $strhtml = '';
+                            $.each(data, function(index, value) {
+                                $strhtml += `
+                                        <li>
+                                            <a href="detail.html?sid=${value.sid}" class="like">
+                                                <span></span>
+                                                <font></font>
+                                            </a>
+                                            <a href="">
+                                                <img class="lazy" data-original="${value.url}">
+                                            </a>
+                                            <p class="goodsname">
+                                            ${value.title}
+                                            </p>
+                                            <p class="goodsprice">
+                                            ￥${value.price}
+                                            </p>
+                                        </li>
+                                    `;
+                            });
+                            $list.html($strhtml);
+                            //懒加载
+                            $("img.lazy").lazyload({ effect: "fadeIn" });
+
+                            //将li元素添加到排序前的数组中。
+                            $('.list li').each(function(index, element) { //element:原生的元素对象
+                                $array_default[index] = $(this); //排序前
+                                $array[index] = $(this); //排序后
+                            });
+                            console.log($array_default);
+                        });
+                    }
+                });
+
+
+                //3.点击按钮进行排序
+                $('button').eq(0).on('click', function() {
+                    //遍历渲染。
+                    $.each($array_default, function(index, value) { //value就是li元素
+                        $list.append(value);
+                    });
+                });
+                $('button').eq(1).on('click', function() {
+                    for (let i = 0; i < $array.length - 1; i++) {
+                        for (let j = 0; j < $array.length - i - 1; j++) {
+                            $prev = parseFloat($array[j].find('span').html().substring(1)); //上一个价格
+                            $next = parseFloat($array[j + 1].find('span').html().substring(1)); //下一个价格
+                            if ($prev > $next) {
+                                //通过价格的比较,交换的是里面的这个li元素
+                                let temp = $array[j];
+                                $array[j] = $array[j + 1];
+                                $array[j + 1] = temp;
+                            }
+                        }
+                    }
+                    //遍历渲染。
+                    $.each($array, function(index, value) { //value就是li元素
+                        $list.append(value);
+                    });
+                });
+
+                $('.button').eq(2).on('click', function() {
+                    for (let i = 0; i < $array.length - 1; i++) {
+                        for (let j = 0; j < $array.length - i - 1; j++) {
+                            $prev = parseFloat($array[j].find('span').html().substring(1)); //上一个价格
+                            $next = parseFloat($array[j + 1].find('span').html().substring(1)); //下一个价格
+                            if ($prev < $next) {
+                                //通过价格的比较,交换的是里面的这个li元素
+                                let temp = $array[j];
+                                $array[j] = $array[j + 1];
+                                $array[j + 1] = temp;
+                            }
+                        }
+                    }
+                    //遍历渲染。
+                    $.each($array, function(index, value) { //value就是li元素
+                        $list.append(value);
+                    });
+                });
+
+
+            });
+        }
+    }
+});
